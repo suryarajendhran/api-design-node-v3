dcb32e1ec889c6db0f530e791084f14cabf949a8
Surya Rajendhran <32726562+suryarajendhran@users.noreply.github.com>
2021-09-04 12:53:53 +0530

    add routes with mockController

--- src/resources/item/item.router.js
+++ src/resources/item/item.router.js
@@ -2,4 +2,20 @@ import { Router } from 'express'
 
 const router = Router()
 
+const mockController = (res, req) => {
+  res.send({ message: 'route works' })
+}
+// /api/item
+router
+  .route('/')
+  .get(mockController)
+  .post(mockController)
+
+// /api/item/:id
+router
+  .route('/:id')
+  .get(mockController)
+  .put(mockController)
+  .delete(mockController)
+
 export default router
